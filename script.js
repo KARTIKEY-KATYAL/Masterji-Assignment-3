a850a2d1665b68cd403f3eaecbcfe9a5661d8df3
Kartikey Katyal <kartikeykatyal2003@gmail.com>
2025-03-17 10:16:19 +0530

    css modified

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function generatenewquote(params) {
-    
+async function fetchqoute(params) {
+    try {
+       const response =  await fetch(
+          "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+        )
+        const data = await response.json();
+        // console.log(data.data.author);
+
+        const quote = document.getElementById('quote')
+        const author = document.getElementById("quoteauthor");
+        quote.innerHTML = `"${data.data.content}"`
+        author.innerHTML = `Author : ${data.data.author}`;
+    } catch (error) {
+        document.getElementById('quote').innerHTML = 'Error: ' + error;
+    }
 }
+
+const newquotebtn = document.getElementById("newquote");
+newquotebtn.addEventListener("click", fetchqoute);
